fd5f6a7970746ab982da916174cb321c94f9ce30
Hunter Belanger <hunter.belanger@gmail.com>
2023-12-31 15:19:49 -0500

    Remove stuff from old diffusion solver.

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,12 @@
-#include <material.hpp>
-#include <geometry.hpp>
-#include <solver.hpp>
-
 #include <exception>
 #include <iostream>
 #include <map>
 #include <string>
+#include <cmath>
+#include <vector>
+#include <memory>
 
 #include <docopt.h>
-
-#include <yaml-cpp/yaml.h>
-
-static const std::string help =
-"Scarabée\n\n"
-
-"Usage:\n"
-"   scarabée <input>\n"
-"   scarabée (-h | --help)\n"
-"   scarabée --version\n\n"
-
-" Options:\n"
-"   <input>       Name of the input file.\n"
-"   --version     Show the version information.\n"
-"   -h --help     Show this help message.\n";
-
-/*
-int main(int argc, char** argv) {
-  // Initialize docopt
-  std::map<std::string, docopt::value> args = docopt::docopt(help, {argv + 1, argv + argc}, true, "Scarabée 0.1.0");
-
-  // Read intput file into YAML node
-  YAML::Node input = YAML::LoadFile(args["<input>"].asString());
-
-  // Read all materials
-  const auto& mats = input["materials"];
-  if (mats.IsSequence() == false) {
-    throw std::runtime_error("materials must be a sequence of material entries.");
-  }
-
-  for (std::size_t m = 0; m < mats.size(); m++) {
-    Material mat(mats[m]);
-    materials.insert({mat.id(), mat});
-  }
-  std::cout << "Made materials...\n";
-
-  // Read the geometry
-  const auto& geom_node = input["geometry"];
-  Geom1D geom(geom_node);
-  std::cout << "Made geometry...\n";
-
-  // Make problem solver
-  StaticSolver1D solver(std::move(geom));
-  std::cout << "Made solver...\n";
-
-  // Solve problem
-  solver.solve();
-
-  return 0;
-}*/
-
 
 #include <self_shielding/macro_flux_spectrum.hpp>
 #include <self_shielding/narrow_resonance.hpp>
@@ -67,11 +15,7 @@ int main(int argc, char** argv) {
 
 #include <PapillonNDL/st_neutron.hpp>
 
-#include <cmath>
-#include <vector>
-#include <memory>
-
-const std::vector<double> xmas_172 {
+const std::vector<double> xmas_172{
     1.00001e-05, 3.00000e-03, 5.00000e-03, 6.90000e-03, 1.00000e-02,
     1.50000e-02, 2.00000e-02, 2.50000e-02, 3.00000e-02, 3.50000e-02,
     4.20000e-02, 5.00000e-02, 5.80000e-02, 6.70000e-02, 7.70000e-02,
@@ -108,13 +52,13 @@ const std::vector<double> xmas_172 {
     6.70320e+06, 8.18731e+06, 1.00000e+07, 1.16183e+07, 1.38403e+07,
     1.49182e+07, 1.73325e+07, 1.96403e+07};
 
-std::pair<std::vector<double>, std::vector<double>> group_xs(const std::vector<double>& group_bounds,
-                                                             const std::function<double(double)>& flux,
-                                                             const std::vector<double>& energy,
-                                                             const std::vector<double>& xs) {
+std::pair<std::vector<double>, std::vector<double>> group_xs(
+    const std::vector<double>& group_bounds,
+    const std::function<double(double)>& flux,
+    const std::vector<double>& energy, const std::vector<double>& xs) {
   std::vector<double> group_bounds_;
   std::vector<double> group_xs;
-  
+
   std::vector<double> flux_vals(energy.size());
   std::vector<double> flux_xs_vals(energy.size());
   for (std::size_t i = 0; i < energy.size(); i++) {
@@ -124,14 +68,16 @@ std::pair<std::vector<double>, std::vector<double>> group_xs(const std::vector<d
 
   // Create functions which will be responsible for the integrations
   pndl::Tabulated1D flux_func(pndl::Interpolation::LinLin, energy, flux_vals);
-  pndl::Tabulated1D flux_xs_func(pndl::Interpolation::LinLin, energy, flux_xs_vals);
+  pndl::Tabulated1D flux_xs_func(pndl::Interpolation::LinLin, energy,
+                                 flux_xs_vals);
 
   // Generate each group constant
-  for (std::size_t i = 0; i < group_bounds.size()-1; i++) {
+  for (std::size_t i = 0; i < group_bounds.size() - 1; i++) {
     double Elow = group_bounds[i];
-    double Ehi = group_bounds[i+1];
+    double Ehi = group_bounds[i + 1];
 
-    double grp_xs = flux_xs_func.integrate(Elow, Ehi) / flux_func.integrate(Elow, Ehi);
+    double grp_xs =
+        flux_xs_func.integrate(Elow, Ehi) / flux_func.integrate(Elow, Ehi);
 
     group_bounds_.push_back(Elow);
     group_xs.push_back(grp_xs);
@@ -144,50 +90,57 @@ std::pair<std::vector<double>, std::vector<double>> group_xs(const std::vector<d
 }
 
 class PlotLayer : public ImApp::Layer {
-  public:
-    PlotLayer() {
-      pndl::ACE ace("U238.293.6.ace");
-      pndl::STNeutron nuc(ace);
+ public:
+  PlotLayer() {
+    pndl::ACE ace("U238.293.6.ace");
+    pndl::STNeutron nuc(ace);
 
-      energy = nuc.total_xs().energy();
-      for (auto& engy : energy) { engy *= 1.E6; }
-
-      MacroFluxSpectrum spec;
-      pndl::Tabulated1D tot_xs(pndl::Interpolation::LinLin, energy, nuc.total_xs().xs());
-      NarrowResonance nr(spec, tot_xs, 1.0);
-
-      std::cout << " START \n";
-      grouped_xss.push_back(group_xs(xmas_172, spec, energy, tot_xs.y()));
-      grouped_xss.push_back(group_xs(xmas_172, nr, energy, tot_xs.y()));
-      std::cout << " STOP \n";
+    energy = nuc.total_xs().energy();
+    for (auto& engy : energy) {
+      engy *= 1.E6;
     }
 
-    void render() override final {
-      ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+    MacroFluxSpectrum spec;
+    pndl::Tabulated1D tot_xs(pndl::Interpolation::LinLin, energy,
+                             nuc.total_xs().xs());
+    NarrowResonance nr(spec, tot_xs, 1.0);
 
-      ImGui::SetNextWindowSize({500, 500}, ImGuiCond_Once);
-      ImGui::Begin("XS Plot");
-      ImPlot::BeginPlot("Grouped XS", ImVec2(-1,-1));
-      ImPlot::SetupAxis(ImAxis_X1, "Energy [eV]");
-      ImPlot::SetupAxisScale(ImAxis_X1, ImPlotScale_Log10);
-      ImPlot::SetupAxis(ImAxis_Y1, "Cross Section [barns]");
-      ImPlot::SetupAxisScale(ImAxis_Y1, ImPlotScale_Log10);
+    std::cout << " START \n";
+    grouped_xss.push_back(group_xs(xmas_172, spec, energy, tot_xs.y()));
+    grouped_xss.push_back(group_xs(xmas_172, nr, energy, tot_xs.y()));
+    std::cout << " STOP \n";
+  }
 
-      ImPlot::PlotLine("Smooth Spectrum", grouped_xss[0].first.data(), grouped_xss[0].second.data(), static_cast<int>(grouped_xss[0].first.size()));
-      ImPlot::PlotLine("NR Spectrum", grouped_xss[1].first.data(), grouped_xss[1].second.data(), static_cast<int>(grouped_xss[1].first.size()));
+  void render() override final {
+    ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
-      ImPlot::EndPlot();
-      ImGui::End();
-    }
+    ImGui::SetNextWindowSize({500, 500}, ImGuiCond_Once);
+    ImGui::Begin("XS Plot");
+    ImPlot::BeginPlot("Grouped XS", ImVec2(-1, -1));
+    ImPlot::SetupAxis(ImAxis_X1, "Energy [eV]");
+    ImPlot::SetupAxisScale(ImAxis_X1, ImPlotScale_Log10);
+    ImPlot::SetupAxis(ImAxis_Y1, "Cross Section [barns]");
+    ImPlot::SetupAxisScale(ImAxis_Y1, ImPlotScale_Log10);
 
-  private:
-    std::vector<double> energy;
-    std::vector<std::pair<std::vector<double>, std::vector<double>>> grouped_xss;
+    ImPlot::PlotLine("Smooth Spectrum", grouped_xss[0].first.data(),
+                     grouped_xss[0].second.data(),
+                     static_cast<int>(grouped_xss[0].first.size()));
+    ImPlot::PlotLine("NR Spectrum", grouped_xss[1].first.data(),
+                     grouped_xss[1].second.data(),
+                     static_cast<int>(grouped_xss[1].first.size()));
+
+    ImPlot::EndPlot();
+    ImGui::End();
+  }
+
+ private:
+  std::vector<double> energy;
+  std::vector<std::pair<std::vector<double>, std::vector<double>>> grouped_xss;
 };
 
 int main() {
   std::unique_ptr<ImApp::Layer> layer = std::make_unique<PlotLayer>();
-  
+
   ImApp::App app(1920, 1080, "Scarabee");
   app.push_layer(std::move(layer));
   app.enable_docking();
