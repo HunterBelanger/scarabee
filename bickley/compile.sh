f887f2698d22c43a1e0b941890b2539cf3ff4830
Hunter Belanger <hunter.belanger@gmail.com>
2024-01-15 19:11:46 -0500

    Remove now undeeded Bickley fortran functions.

--- bickley/compile.sh
+++
@@ -1,3 +0,0 @@
-gcc -g -c main.c
-gfortran -g main.o dbskin.f  d1mach.f dbdiff.f dbkias.f dbkisr.f dexint.f dgamrn.f dhkseq.f dpsixn.f fdump.f i1mach.f j4save.f xercnt.f xerhlt.f xermsg.f xerprn.f xersve.f xgetua.f ki3.f90
-#gfortran dbskin.f  d1mach.f dbdiff.f dbkias.f dbkisr.f dexint.f dgamrn.f dhkseq.f dpsixn.f fdump.f i1mach.f j4save.f xercnt.f xerhlt.f xermsg.f xerprn.f xersve.f xgetua.f ki3.f90
