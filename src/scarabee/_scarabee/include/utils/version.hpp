bfe7afef0d06328d0765e09e75e7a34e795f8c40
Hunter Belanger <hunter.belanger@gmail.com>
2024-07-11 22:10:22 -0400

    Update version info in package

--- src/scarabee/_scarabee/include/utils/version.hpp
+++ src/scarabee/_scarabee/include/utils/version.hpp
@@ -7,7 +7,7 @@ const unsigned int VERSION_MAJOR{0};
 const unsigned int VERSION_MINOR{1};
 const unsigned int VERSION_PATCH{0};
 const bool VERSION_DEVELOPMENT{false};
-const char* VERSION_STRING{"0.3.1"};
+const char* VERSION_STRING{"0.1.0"};
 
 }  // namespace scarabee
 
