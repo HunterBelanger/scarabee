5be56088a0b38b5d13d82242e217a34f490b3f07
Hunter Belanger <hunter.belanger@gmail.com>
2024-11-24 22:21:04 -0500

    Begins classes for compressed xs data

---
+++ src/scarabee/_scarabee/include/data/xs1d.hpp
@@ -0,0 +1,103 @@
+#ifndef SCARABEE_XS1D_H
+#define SCARABEE_XS1D_H
+
+#include <utils/logging.hpp>
+#include <utils/scarabee_exception.hpp>
+
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xview.hpp>
+
+#include <cmath>
+
+namespace scarabee {
+
+class XS1D {
+ public:
+  XS1D(const xt::xtensor<double, 1>& E) : xs_(E) {}
+
+  double operator()(const std::size_t g) const {
+    if (g >= xs_.size()) {
+      return 0.;
+    }
+
+    return xs_(g);
+  }
+
+  void set_value(const std::size_t g, const double v) {
+    if (g >= xs_.size()) {
+      const auto mssg = "Group index is out of range.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    xs_(g) = v;
+  }
+
+  std::size_t ngroups() const { return xs_.size(); }
+
+  void resize(const std::size_t NG) {
+    xt::xtensor<double, 1> tmp = xs_;
+    xs_ = xt::zeros<double>({NG});
+
+    for (std::size_t g = 0; g < std::min(NG, tmp.size()); g++) {
+      xs_(g) = tmp(g);
+    }
+  }
+
+  XS1D& operator+=(const XS1D xs2) {
+    if (this->ngroups() < xs2.ngroups()) {
+      this->resize(xs2.ngroups());
+    }
+
+    for (std::size_t g = 0; g < this->ngroups(); g++) xs_(g) += xs2(g);
+  }
+
+  XS1D& operator-=(const XS1D xs2) {
+    if (this->ngroups() < xs2.ngroups()) {
+      this->resize(xs2.ngroups());
+    }
+
+    for (std::size_t g = 0; g < this->ngroups(); g++) xs_(g) -= xs2(g);
+  }
+
+  XS1D& operator*=(const double v) { xs_ *= v; }
+
+  XS1D& operator/=(const double v) { xs_ /= v; }
+
+  XS1D operator+(const XS1D& xs2) const {
+    XS1D out(*this);
+    out += xs2;
+    return out;
+  }
+
+  XS1D operator-(const XS1D& xs2) const {
+    XS1D out(*this);
+    out -= xs2;
+    return out;
+  }
+
+  XS1D operator*(const double v) const {
+    XS1D out(*this);
+    out *= v;
+    return out;
+  }
+
+  XS1D operator/(const double v) const {
+    XS1D out(*this);
+    out /= v;
+    return out;
+  }
+
+ private:
+  xt::xtensor<double, 1> xs_;
+};
+
+inline XS1D operator*(const double v, const XS1D& xs) {
+  XS1D out(xs);
+  out *= v;
+  return out;
+}
+
+}  // namespace scarabee
+
+#endif
---
+++ src/scarabee/_scarabee/include/data/xs2d.hpp
@@ -0,0 +1,435 @@
+#ifndef SCARABEE_XS2D_H
+#define SCARABEE_XS2D_H
+
+#include <utils/logging.hpp>
+#include <utils/scarabee_exception.hpp>
+
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xview.hpp>
+
+#include <cmath>
+
+namespace scarabee {
+
+class XS2D {
+ public:
+  XS2D(const xt::xtensor<double, 2>& xs,
+       const xt::xtensor<std::size_t, 2>& packing)
+      : xs_(xs), packing_(packing) {
+    // Make sure packing is valid
+    if (packing_.shape()[1] != 3) {
+      const auto mssg =
+          "Second dimension of packing array must have a length of 3.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    for (std::size_t g = 0; g < packing_.shape()[0]; g++) {
+      if (packing_(g, 1) > packing_(g, 2)) {
+        const auto mssg =
+            "The lowest outgoing group is larger than the highest outgoing "
+            "group.";
+        spdlog::error(mssg);
+        throw ScarabeeException(mssg);
+      }
+    }
+
+    // Make sure the shapes align
+    if (xs_.shape()[0] == 0) {
+      const auto mssg = "Must provide at least the P0 scattering moment data.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    const std::size_t NG = packing_.shape()[0] - 1;
+    const std::size_t NDAT =
+        packing_(NG, 0) + packing_(NG, 2) + 1 - packing_(NG, 1);
+
+    if (xs.shape()[1] != NDAT) {
+      const auto mssg =
+          "Length of the cross section data array does not agree with length "
+          "indicated by packing scheme.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+  }
+
+  XS2D(const xt::xtensor<double, 2>& Es) : xs_(), packing_() {
+    if (Es.shape()[0] != Es.shape()[1]) {
+      const auto mssg = "Scattering matrix is not square.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    const std::size_t NG = Es.shape()[0];
+
+    packing_.reshape({NG, 3});
+    packing_.fill(0);
+
+    for (std::size_t g = 0; g < NG; g++) {
+      // Get the min and max outgoing group for each incident group
+      std::size_t g_min = 0;
+      for (std::size_t gg = 0; gg < NG; gg++) {
+        if (Es(g, gg) != 0.) {
+          g_min = gg;
+          break;
+        }
+      }
+
+      std::size_t g_max = NG - 1;
+      for (int gg = static_cast<int>(NG) - 1; gg >= 0; gg--) {
+        if (Es(g, static_cast<std::size_t>(gg)) != 0.) {
+          g_max = static_cast<std::size_t>(gg);
+        }
+      }
+
+      if (g == 0) {
+        packing_(g, 0) = 0;
+      } else {
+        packing_(g, 0) = packing_(g - 1, 0) + g_max + 1 - g_min;
+      }
+      packing_(g, 1) = g_min;
+      packing_(g, 2) = g_max;
+    }
+
+    const std::size_t NDAT =
+        packing_(NG - 1, 0) + packing_(NG - 1, 2) + 1 - packing_(NG - 1, 1);
+    xs_ = xt::zeros<double>({static_cast<std::size_t>(1), NDAT});
+
+    for (std::size_t g = 0; g < NG; g++) {
+      const auto strt = packing_(g, 0);
+      const auto g_min = packing_(g, 1);
+      const auto g_max = packing_(g, 2);
+
+      for (std::size_t gg = g_min; gg <= g_max; gg++) {
+        xs_(0, strt + gg - g_min) += Es(g, gg);
+      }
+    }
+  }
+
+  XS2D(const xt::xtensor<double, 3>& Es) : xs_(), packing_() {
+    if (Es.shape()[1] != Es.shape()[2]) {
+      const auto mssg = "Scattering matrix is not square.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+    const std::size_t NG = Es.shape()[1];
+
+    packing_.reshape({NG, 3});
+    packing_.fill(0);
+
+    for (std::size_t g = 0; g < NG; g++) {
+      // Get the min and max outgoing group for each incident group
+      std::size_t g_min = 0;
+      for (std::size_t gg = 0; gg < NG; gg++) {
+        if (Es(0, g, gg) != 0.) {
+          g_min = gg;
+          break;
+        }
+      }
+
+      std::size_t g_max = NG - 1;
+      for (int gg = static_cast<int>(NG) - 1; gg >= 0; gg--) {
+        if (Es(0, g, static_cast<std::size_t>(gg)) != 0.) {
+          g_max = static_cast<std::size_t>(gg);
+        }
+      }
+
+      if (g == 0) {
+        packing_(g, 0) = 0;
+      } else {
+        packing_(g, 0) = packing_(g - 1, 0) + g_max + 1 - g_min;
+      }
+      packing_(g, 1) = g_min;
+      packing_(g, 2) = g_max;
+    }
+
+    const std::size_t NDAT =
+        packing_(NG - 1, 0) + packing_(NG - 1, 2) + 1 - packing_(NG - 1, 1);
+    xs_ = xt::zeros<double>({Es.shape()[0], NDAT});
+
+    for (std::size_t l = 0; l < Es.shape()[0]; l++) {
+      for (std::size_t g = 0; g < NG; g++) {
+        const auto strt = packing_(g, 0);
+        const auto g_min = packing_(g, 1);
+        const auto g_max = packing_(g, 2);
+
+        for (std::size_t gg = g_min; gg <= g_max; gg++) {
+          xs_(l, strt + gg - g_min) += Es(l, g, gg);
+        }
+      }
+    }
+  }
+
+  std::size_t ngroups() const { return packing_.shape()[0]; }
+
+  bool anisotropic() const { return xs_.shape()[0] > 1; }
+
+  std::size_t max_legendre_order() const { return xs_.shape()[0] - 1; }
+
+  double operator()(const std::size_t l, const std::size_t g) const {
+    if (g >= ngroups()) {
+      const auto mssg = "Incident group index out of range.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    if (l > this->max_legendre_order()) {
+      return 0.;
+    }
+
+    const std::size_t strt = packing_(g, 0);
+    const std::size_t g_min = packing_(g, 1);
+    const std::size_t g_max = packing_(g, 2);
+    const std::size_t max = strt + 1 + g_max - g_min;
+
+    double Es = 0.;
+    for (std::size_t g = strt; g < max; g++) {
+      Es += xs_(l, g);
+    }
+
+    return Es;
+  }
+
+  double operator()(const std::size_t l, const std::size_t gin,
+                    const std::size_t gout) const {
+    if (gin >= ngroups()) {
+      const auto mssg = "Incident group index out of range.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    if (gout >= ngroups()) {
+      const auto mssg = "Outgoing group index out of range.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    if (l > this->max_legendre_order()) {
+      return 0.;
+    }
+
+    const std::size_t strt = packing_(gin, 0);
+    const std::size_t g_min = packing_(gin, 1);
+    const std::size_t g_max = packing_(gin, 2);
+
+    if ((gout < g_min) || (gout > g_max)) return 0.;
+
+    return xs_(l, strt + gout - g_min);
+  }
+
+  void set_value(const std::size_t l, const std::size_t gin,
+                 const std::size_t gout, double v) {
+    if (l > this->max_legendre_order()) {
+      const auto mssg = "Legendre order index out of range.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    if (gin >= ngroups()) {
+      const auto mssg = "Incident group index out of range.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    if (gout >= ngroups()) {
+      const auto mssg = "Outgoing group index out of range.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    const std::size_t strt = packing_(gin, 0);
+    const std::size_t g_min = packing_(gin, 1);
+    const std::size_t g_max = packing_(gin, 2);
+
+    if ((gout < g_min) || (gout > g_max)) {
+      const auto mssg =
+          "The desired incident group -> outgoing group pair is not in the "
+          "sparse matrix.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    xs_(l, strt + gout - g_min) = v;
+  }
+
+  void repack_to_be_compatible(const xt::xtensor<std::size_t, 2>& packing2) {
+    if (ngroups() != packing2.shape()[0]) {
+      const auto mssg = "Data packing schemes have different number of groups.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    if (packing2.shape()[1] != 3) {
+      const auto mssg = "Provided packing scheme does not have a valid shape.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    // First, check if the two packing strategies are the same, or already
+    // compatible. If so, we don't need to do anything.
+    bool not_compat = false;
+    for (std::size_t g = 0; g < ngroups(); g++) {
+      if (packing_(g, 1) > packing2(g, 1)) {
+        not_compat = true;
+        break;
+      } else if (packing_(g, 2) < packing2(g, 2)) {
+        not_compat = true;
+        break;
+      }
+    }
+    if (not_compat == true) return;
+
+    // Unfortunately, they are not compatible. We need to re-pack the data.
+    auto new_data = packing_;
+
+    for (std::size_t g = 0; g < ngroups(); g++) {
+      // Set start index based on previous group data
+      if (g == 0) {
+        new_data(g, 0) = 0;
+      } else {
+        new_data(g, 0) =
+            new_data(g - 1, 0) + new_data(g - 1, 2) + 1 - new_data(g - 1, 1);
+      }
+
+      // Find the new min and max outgoing groups for incident group g
+      new_data(g, 1) = std::min(packing_(g, 1), packing2(g, 1));
+      new_data(g, 2) = std::max(packing_(g, 2), packing2(g, 2));
+    }
+
+    // Get the required length of the data array
+    const std::size_t NDAT = new_data(ngroups() - 1, 0) +
+                             new_data(ngroups() - 1, 2) + 1 -
+                             new_data(ngroups() - 1, 1);
+
+    // Allocate new array for data
+    xt::xtensor<double, 2> new_xs = xt::zeros<double>({xs_.shape()[0], NDAT});
+
+    // Fill new array
+    for (std::size_t l = 0; l <= max_legendre_order(); l++) {
+      for (std::size_t g = 0; g < ngroups(); g++) {
+        const auto strt = new_data(g, 0);
+        const auto g_min = new_data(g, 1);
+        const auto g_max = new_data(g, 2);
+
+        for (std::size_t gg = g_min; gg <= g_max; gg++) {
+          new_xs(l, strt + gg - g_min) = (*this)(l, g, gg);
+        }
+      }
+    }
+
+    xs_ = new_xs;
+    packing_ = new_data;
+  }
+
+  XS2D& operator+=(const XS2D& xs2) {
+    if (ngroups() != xs2.ngroups()) {
+      const auto mssg =
+          "Cross section matrices have different number of groups.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    // Make the packing format compatible
+    this->repack_to_be_compatible(xs2.packing_);
+
+    // Must deal with unequal legendre orders
+    if (max_legendre_order() != xs2.max_legendre_order()) {
+      const std::size_t NL = std::max(xs_.shape()[0], xs2.xs_.shape()[0]);
+      xt::xtensor<double, 2> new_xs = xt::zeros<double>({NL, xs_.shape()[1]});
+
+      xt::view(new_xs, xt::range(0, xs_.shape()[0]), xt::all()) = xs_;
+      xs_ = new_xs;
+    }
+
+    // Shapes are now completely equivalent.
+    for (std::size_t l = 0; l <= max_legendre_order(); l++) {
+      for (std::size_t g = 0; g < ngroups(); g++) {
+        const auto strt = packing_(g, 0);
+        const auto g_min = packing_(g, 1);
+        const auto g_max = packing_(g, 2);
+
+        for (std::size_t gg = g_min; gg <= g_max; gg++) {
+          xs_(l, strt + gg - g_min) += xs2(l, g, gg);
+        }
+      }
+    }
+  }
+
+  XS2D& operator-=(const XS2D& xs2) {
+    if (ngroups() != xs2.ngroups()) {
+      const auto mssg =
+          "Cross section matrices have different number of groups.";
+      spdlog::error(mssg);
+      throw ScarabeeException(mssg);
+    }
+
+    // Make the packing format compatible
+    this->repack_to_be_compatible(xs2.packing_);
+
+    // Must deal with unequal legendre orders
+    if (max_legendre_order() != xs2.max_legendre_order()) {
+      const std::size_t NL = std::max(xs_.shape()[0], xs2.xs_.shape()[0]);
+      xt::xtensor<double, 2> new_xs = xt::zeros<double>({NL, xs_.shape()[1]});
+
+      xt::view(new_xs, xt::range(0, xs_.shape()[0]), xt::all()) = xs_;
+      xs_ = new_xs;
+    }
+
+    // Shapes are now completely equivalent.
+    for (std::size_t l = 0; l <= max_legendre_order(); l++) {
+      for (std::size_t g = 0; g < ngroups(); g++) {
+        const auto strt = packing_(g, 0);
+        const auto g_min = packing_(g, 1);
+        const auto g_max = packing_(g, 2);
+
+        for (std::size_t gg = g_min; gg <= g_max; gg++) {
+          xs_(l, strt + gg - g_min) -= xs2(l, g, gg);
+        }
+      }
+    }
+  }
+
+  XS2D operator+(const XS2D& xs2) {
+    XS2D out(*this);
+    out += xs2;
+    return out;
+  }
+
+  XS2D operator-(const XS2D& xs2) {
+    XS2D out(*this);
+    out += xs2;
+    return out;
+  }
+
+  XS2D& operator*=(const double v) { xs_ *= v; }
+
+  XS2D& operator/=(const double v) { xs_ /= v; }
+
+  XS2D operator*(const double v) const {
+    XS2D out(*this);
+    out *= v;
+    return out;
+  }
+
+  XS2D operator/(const double v) const {
+    XS2D out(*this);
+    out /= v;
+    return out;
+  }
+
+ private:
+  xt::xtensor<double, 2> xs_;
+  xt::xtensor<std::size_t, 2>
+      packing_;  // First index incident group, second (data start, g_low, g_hi)
+};
+
+inline XS2D operator*(const double v, const XS2D& xs) {
+  XS2D out(xs);
+  out *= v;
+  return out;
+}
+
+}  // namespace scarabee
+
+#endif
