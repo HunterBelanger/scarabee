b318e8a961ba607a270eb30ffdb9e564d5aff569
Hunter Belanger <hunter.belanger@gmail.com>
2024-12-03 22:27:56 -0500

    Applies a python format

--- src/scarabee/__init__.py
+++ src/scarabee/__init__.py
@@ -3,10 +3,9 @@ import _scarabee
 
 from .reflector import *
 
-__author__     = _scarabee.__author__
-__copyright__  = _scarabee.__copyright__
-__license__    = _scarabee.__license__
+__author__ = _scarabee.__author__
+__copyright__ = _scarabee.__copyright__
+__license__ = _scarabee.__license__
 __maintainer__ = _scarabee.__maintainer__
-__email__      = _scarabee.__email__
-__version__    = _scarabee.__version__
-
+__email__ = _scarabee.__email__
+__version__ = _scarabee.__version__
