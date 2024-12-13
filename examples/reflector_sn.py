0a679a9f8c921a49053f10c10a63e3b6b53f8d2c
Hunter Belanger <hunter.belanger@gmail.com>
2024-12-12 20:20:47 -0500

    Fixes isotropic ReflectorSN example

--- examples/reflector_sn.py
+++ examples/reflector_sn.py
@@ -1,23 +1,23 @@
 from scarabee import *
 import numpy as np
 
-Et = np.array([0.32640])
-Ea = np.array([0.101184])
-Ef = np.array([0.081600])
-nu = np.array([3.24])
-chi = np.array([1.])
-Es = np.array([[0.225216]])
-xs = CrossSection(Et, Ea, Es, Ef, nu*Ef, chi)
-L = 1.853722
-
-#Et = np.array([0.54628])
-#Ea = np.array([0.54628 - 0.464338])
-#Ef = np.array([0.054628])
-#nu = np.array([1.7])
+#Et = np.array([0.32640])
+#Ea = np.array([0.101184])
+#Ef = np.array([0.081600])
+#nu = np.array([3.24])
 #chi = np.array([1.])
-#Es = np.array([[0.464338]])
+#Es = np.array([[0.225216]])
 #xs = CrossSection(Et, Ea, Es, Ef, nu*Ef, chi)
-#L = 10.371065
+#L = 1.853722
+
+Et = np.array([0.54628])
+Ea = np.array([0.54628 - 0.464338])
+Ef = np.array([0.054628])
+nu = np.array([1.7])
+chi = np.array([1.])
+Es = np.array([[0.464338]])
+xs = CrossSection(Et, Ea, Es, Ef, nu*Ef, chi)
+L = 10.371065
 
 #Et = np.array([0.33588, 0.54628])
 #Ea = np.array([0.33588 - (0.3198 + 0.004555), 0.54628 - 0.42410])
@@ -29,7 +29,7 @@ L = 1.853722
 #xs = CrossSection(Et, Ea, Es, Ef, nu*Ef, chi)
 #L = 846.632726
 
-N = 1
+N = 100000
 
 dx = N * [L/N]
 xsary = N * [xs]
