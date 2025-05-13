3e7fe0817c529fc95b08ab9835b3ed83282a5078
Hunter Belanger <hunter.belanger@gmail.com>
2025-05-13 15:34:23 -0400

    Maybe fixed ensleeve functions ? Need to test.

--- src/scarabee/reseau/_ensleeve.py
+++ src/scarabee/reseau/_ensleeve.py
@@ -12,30 +12,43 @@ def _ensleeve_quarter(
     #  ---|
     #   PG|
 
-    # First make the gap on the right hand side
-    right_gap_Ny = round(
-        (pin_geom.dy + gap_width - (0.5 * pitch + gap_width)) / (0.5 * pitch)
-    )
-    right_gap_dy = (
-        pin_geom.dy + gap_width - (0.5 * pitch + gap_width)
-    ) / right_gap_Ny  # Should be exactly 0.5*pitch !
-    right_gap = Cartesian2D(
-        [gap_width], right_gap_Ny * [right_gap_dy] + [0.5 * pitch + gap_width]
-    )
-    right_gap_tiles = [EmptyCell(gap_xs, gap_width, 0.5 * pitch + gap_width)]
-    for i in range(right_gap_Ny):
-        right_gap_tiles.append(EmptyCell(gap_xs, gap_width, right_gap_dy))
+    # First make the gap on the right hand side going from bottom to top
+    LY = pin_geom.dy + gap_width
+    dy = []
+    right_gap_tiles = []
+    while LY > 0.:
+        ly = 0.5*pitch
+        break_after = False
+        if LY > 0.5*pitch and LY < pitch:
+            ly = LY
+            break_after = True
+        LY -= ly
+        dy.append(ly)
+        right_gap_tiles.append(EmptyCell(gap_xs, gap_width, ly))
+        if break_after:
+            break
+    right_gap_tiles.reverse()
+    right_gap = Cartesian2D([gap_width], dy)
     right_gap.set_tiles(right_gap_tiles)
-
-    # Now make the gap on the top
-    top_gap_Nx = round(pin_geom.dx / (0.5 * pitch))
-    top_gap_dx = pin_geom.dx / top_gap_Nx  # Should be exactly 0.5*pitch !
-    top_gap = Cartesian2D(top_gap_Nx * [top_gap_dx], [gap_width])
+    
+    # Make top gap 
+    LX = pin_geom.dx
+    dx = []
     top_gap_tiles = []
-    for i in range(top_gap_Nx):
-        top_gap_tiles.append(EmptyCell(gap_xs, top_gap_dx, gap_width))
+    while LX > 0.:
+        lx = 0.5*pitch
+        break_after = False
+        if LX > 0.5*pitch and LX < pitch:
+            lx = LX
+            break_after = True
+        LX -= lx
+        dx.append(lx)
+        top_gap_tiles.append(EmptyCell(gap_xs, lx, gap_width))
+        if break_after:
+            break
+    top_gap = Cartesian2D(dx, [gap_width])
     top_gap.set_tiles(top_gap_tiles)
-
+    
     # Make the temp geometry which is the pin_geom with the top gap
     temp_geom = Cartesian2D([pin_geom.dx], [pin_geom.dy, gap_width])
     temp_geom.set_tiles([top_gap, pin_geom])
@@ -59,45 +72,63 @@ def _ensleeve_half(
 ) -> Tuple[Cartesian2D, List[int]]:
     #  |--|
     #  |PG|
-
-    # First make the gap on the right hand side
-    right_gap_Ny = round(
-        (pin_geom.dy + gap_width - (0.5 * pitch + gap_width)) / (0.5 * pitch)
-    )
-    right_gap_dy = (
-        pin_geom.dy + gap_width - (0.5 * pitch + gap_width)
-    ) / right_gap_Ny  # Should be exactly 0.5*pitch !
-    right_gap = Cartesian2D(
-        [gap_width], right_gap_Ny * [right_gap_dy] + [0.5 * pitch + gap_width]
-    )
-    right_gap_tiles = [EmptyCell(gap_xs, gap_width, 0.5 * pitch + gap_width)]
-    for i in range(right_gap_Ny):
-        right_gap_tiles.append(EmptyCell(gap_xs, gap_width, right_gap_dy))
+    
+    # First make the gap on the right hand side going from bottom to top
+    LY = pin_geom.dy + gap_width
+    dy = []
+    right_gap_tiles = []
+    while LY > 0.:
+        ly = 0.5*pitch
+        break_after = False
+        if LY > 0.5*pitch and LY < pitch:
+            ly = LY
+            break_after = True
+        LY -= ly
+        dy.append(ly)
+        right_gap_tiles.append(EmptyCell(gap_xs, gap_width, ly))
+        if break_after:
+            break
+    right_gap_tiles.reverse()
+    right_gap = Cartesian2D([gap_width], dy)
     right_gap.set_tiles(right_gap_tiles)
 
-    # Make the gap on the left hand side
-    left_gap_Ny = round(
-        (pin_geom.dy + gap_width - (0.5 * pitch + gap_width)) / (0.5 * pitch)
-    )
-    left_gap_dy = (
-        pin_geom.dy + gap_width - (0.5 * pitch + gap_width)
-    ) / left_gap_Ny  # Should be exactly 0.5*pitch !
-    left_gap = Cartesian2D(
-        [gap_width], left_gap_Ny * [left_gap_dy] + [0.5 * pitch + gap_width]
-    )
-    left_gap_tiles = [EmptyCell(gap_xs, gap_width, 0.5 * pitch + gap_width)]
-    for i in range(left_gap_Ny):
-        left_gap_tiles.append(EmptyCell(gap_xs, gap_width, left_gap_dy))
+    # Make the gap on the left hand side going from bottom to top
+    LY = pin_geom.dy + gap_width
+    dy = []
+    left_gap_tiles = []
+    while LY > 0.:
+        ly = 0.5*pitch
+        break_after = False
+        if LY > 0.5*pitch and LY < pitch:
+            ly = LY
+            break_after = True
+        LY -= ly
+        dy.append(ly)
+        left_gap_tiles.append(EmptyCell(gap_xs, gap_width, ly))
+        if break_after:
+            break
+    left_gap_tiles.reverse()
+    left_gap = Cartesian2D([gap_width], dy)
     left_gap.set_tiles(left_gap_tiles)
 
-    # Now make the gap on the top
-    top_gap_Nx = round(pin_geom.dx / (0.5 * pitch))
-    top_gap_dx = pin_geom.dx / top_gap_Nx  # Should be exactly 0.5*pitch !
-    top_gap = Cartesian2D(top_gap_Nx * [top_gap_dx], [gap_width])
+    # Make top gap 
+    LX = pin_geom.dx
+    dx = []
     top_gap_tiles = []
-    for i in range(top_gap_Nx):
-        top_gap_tiles.append(EmptyCell(gap_xs, top_gap_dx, gap_width))
+    while LX > 0.:
+        lx = 0.5*pitch
+        break_after = False
+        if LX > 0.5*pitch and LX < pitch:
+            lx = LX
+            break_after = True
+        LX -= lx
+        dx.append(lx)
+        top_gap_tiles.append(EmptyCell(gap_xs, lx, gap_width))
+        if break_after:
+            break
+    top_gap = Cartesian2D(dx, [gap_width])
     top_gap.set_tiles(top_gap_tiles)
+
 
     # Make the temp geometry which is the pin_geom with the top gap
     temp_geom = Cartesian2D([pin_geom.dx], [pin_geom.dy, gap_width])
@@ -128,62 +159,83 @@ def _ensleeve_full(
     #  |PG|
     #  |--|
 
-    # First make the gap on the right hand side
-    right_gap_Ny = round(
-        (pin_geom.dy + 2.0 * gap_width - 2.0 * (0.5 * pitch + gap_width))
-        / (0.5 * pitch)
-    )
-    right_gap_dy = (
-        pin_geom.dy + 2.0 * gap_width - 2.0 * (0.5 * pitch + gap_width)
-    ) / right_gap_Ny  # Should be exactly 0.5*pitch !
-    right_gap = Cartesian2D(
-        [gap_width],
-        [0.5 * pitch + gap_width]
-        + right_gap_Ny * [right_gap_dy]
-        + [0.5 * pitch + gap_width],
-    )
-    right_gap_tiles = [EmptyCell(gap_xs, gap_width, 0.5 * pitch + gap_width)]
-    for i in range(right_gap_Ny):
-        right_gap_tiles.append(EmptyCell(gap_xs, gap_width, right_gap_dy))
-    right_gap_tiles.append(EmptyCell(gap_xs, gap_width, 0.5 * pitch + gap_width))
+    # First make the gap on the right hand side going from bottom to top
+    LY = pin_geom.dy + 2.*gap_width
+    extra_y = 0.5*(LY - 0.5*pitch*round(LY / (0.5*pitch)))
+    dy = [0.5*pitch + extra_y]
+    right_gap_tiles = [EmptyCell(gap_xs, gap_width, 0.5*pitch+extra_y)]
+    LY -= 0.5*pitch + extra_y
+    while LY > 0.:
+        ly = 0.5*pitch
+        break_after = False
+        if LY > 0.5*pitch and LY < pitch:
+            ly = LY
+            break_after = True
+        LY -= ly
+        dy.append(ly)
+        right_gap_tiles.append(EmptyCell(gap_xs, gap_width, ly))
+        if break_after:
+            break
+    right_gap_tiles.reverse()
+    right_gap = Cartesian2D([gap_width], dy)
     right_gap.set_tiles(right_gap_tiles)
 
-    # Make the gap on the left hand side
-    left_gap_Ny = round(
-        (pin_geom.dy + 2.0 * gap_width - 2.0 * (0.5 * pitch + gap_width))
-        / (0.5 * pitch)
-    )
-    left_gap_dy = (
-        pin_geom.dy + 2.0 * gap_width - 2.0 * (0.5 * pitch + gap_width)
-    ) / left_gap_Ny  # Should be exactly 0.5*pitch !
-    left_gap = Cartesian2D(
-        [gap_width],
-        [0.5 * pitch + gap_width]
-        + left_gap_Ny * [left_gap_dy]
-        + [0.5 * pitch + gap_width],
-    )
-    left_gap_tiles = [EmptyCell(gap_xs, gap_width, 0.5 * pitch + gap_width)]
-    for i in range(left_gap_Ny):
-        left_gap_tiles.append(EmptyCell(gap_xs, gap_width, left_gap_dy))
-    left_gap_tiles.append(EmptyCell(gap_xs, gap_width, 0.5 * pitch + gap_width))
+    # Make the gap on the left hand side going from bottom to top
+    LY = pin_geom.dy + 2.*gap_width
+    LY = pin_geom.dy + 2.*gap_width
+    extra_y = 0.5*(LY - 0.5*pitch*round(LY / (0.5*pitch)))
+    dy = [0.5*pitch + extra_y]
+    left_gap_tiles = [EmptyCell(gap_xs, gap_width, 0.5*pitch+extra_y)]
+    LY -= 0.5*pitch + extra_y
+    while LY > 0.:
+        ly = 0.5*pitch
+        break_after = False
+        if LY > 0.5*pitch and LY < pitch:
+            ly = LY
+            break_after = True
+        LY -= ly
+        dy.append(ly)
+        left_gap_tiles.append(EmptyCell(gap_xs, gap_width, ly))
+        if break_after:
+            break
+    left_gap_tiles.reverse()
+    left_gap = Cartesian2D([gap_width], dy)
     left_gap.set_tiles(left_gap_tiles)
 
-    # Now make the gap on the top
-    top_gap_Nx = round(pin_geom.dx / (0.5 * pitch))
-    top_gap_dx = pin_geom.dx / top_gap_Nx  # Should be exactly 0.5*pitch !
-    top_gap = Cartesian2D(top_gap_Nx * [top_gap_dx], [gap_width])
+    # Make top gap 
+    LX = pin_geom.dx
+    dx = []
     top_gap_tiles = []
-    for i in range(top_gap_Nx):
-        top_gap_tiles.append(EmptyCell(gap_xs, top_gap_dx, gap_width))
+    while LX > 0.:
+        lx = 0.5*pitch
+        break_after = False
+        if LX > 0.5*pitch and LX < pitch:
+            lx = LX
+            break_after = True
+        LX -= lx
+        dx.append(lx)
+        top_gap_tiles.append(EmptyCell(gap_xs, lx, gap_width))
+        if break_after:
+            break
+    top_gap = Cartesian2D(dx, [gap_width])
     top_gap.set_tiles(top_gap_tiles)
 
-    # Now make the gap on the bottom
-    bot_gap_Nx = round(pin_geom.dx / (0.5 * pitch))
-    bot_gap_dx = pin_geom.dx / bot_gap_Nx  # Should be exactly 0.5*pitch !
-    bot_gap = Cartesian2D(bot_gap_Nx * [bot_gap_dx], [gap_width])
+    # Make bottom gap 
+    LX = pin_geom.dx
+    dx = []
     bot_gap_tiles = []
-    for i in range(bot_gap_Nx):
-        bot_gap_tiles.append(EmptyCell(gap_xs, bot_gap_dx, gap_width))
+    while LX > 0.:
+        lx = 0.5*pitch
+        break_after = False
+        if LX > 0.5*pitch and LX < pitch:
+            lx = LX
+            break_after = True
+        LX -= lx
+        dx.append(lx)
+        top_gap_tiles.append(EmptyCell(gap_xs, lx, gap_width))
+        if break_after:
+            break
+    bot_gap = Cartesian2D(dx, [gap_width])
     bot_gap.set_tiles(bot_gap_tiles)
 
     # Make the temp geometry which is the pin_geom with the top gap
