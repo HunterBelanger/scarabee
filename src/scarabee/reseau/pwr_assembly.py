7f1b75cdbfa7e4e617eb34bb9d71d7c301e61063
Hunter Belanger <hunter.belanger@gmail.com>
2025-06-24 17:02:14 -0400

    Computes moderator volume fraction. Must verify fractions of fuel and moderator !

--- src/scarabee/reseau/pwr_assembly.py
+++ src/scarabee/reseau/pwr_assembly.py
@@ -1,3 +1,4 @@
+from scarabee.reseau.burnable_poison_rod import BurnablePoisonRod
 from .fuel_pin import FuelPin
 from .guide_tube import GuideTube
 from .critical_leakage import CriticalLeakage
@@ -116,6 +117,8 @@ class PWRAssembly:
         Spacing between fuel assemblies.
     fuel_volume_fraction : float
         Fraction of the assembly occupied by fuel.
+    moderator_volume_fraction : float
+        Fraction of the assembly occupied by moderator.
     linear_power : float
         Linear power density of the full assembly in kW/cm.
     initial_heavy_metal_linear_mass : float
@@ -312,6 +315,10 @@ class PWRAssembly:
                         "Grid sleeve width must be < the assembly gap width."
                     )
 
+        # Can only be computed after spacer grid and grid sleeve processing 
+        self._moderator_volume_fraction: float = 0.0
+        self._compute_moderator_volume_fraction()
+
         # Get moderator parameters
         if boron_ppm < 0.0:
             raise ValueError("Boron concentration must be >= 0.")
@@ -484,6 +491,10 @@ class PWRAssembly:
     @property
     def fuel_volume_fraction(self) -> float:
         return self._fuel_volume_fraction
+
+    @property
+    def moderator_volume_fraction(self) -> float:
+        return self._moderator_volume_fraction
 
     @property
     def spacer_grid_width(self) -> Optional[float]:
@@ -880,8 +891,89 @@ class PWRAssembly:
                     pin_volume *= 0.5
 
                 sum_volume += pin_volume
+        
+        if self.symmetry == Symmetry.Quarter:
+            sum_volume *= 4.
+        elif self.symmetry == Symmetry.Half:
+            sum_volume *= 2.
 
         self._fuel_volume_fraction = sum_volume / (
+            self.assembly_pitch * self.assembly_pitch
+        )
+
+    def _compute_moderator_volume_fraction(self) -> None:
+        """
+        Computes the fraction of the assembly which is moderator.
+        """
+        sum_volume = 0.0
+        
+        pin_cell_volume = self.pitch ** 2.
+        if self.spacer_grid_width is not None:
+            pin_cell_volume = (self.pitch - self.spacer_grid_width)**2.
+
+        for j in range(len(self.cells)):
+            for i in range(len(self.cells[j])):
+                cell = self.cells[j][i]
+
+                # Start with volume of moderator in a pin cell
+                cell_mod_volume = pin_cell_volume
+
+                if isinstance(cell, FuelPin):
+                    # Remove volume of pin
+                    cell_mod_volume -= np.pi * cell.clad_radius * cell.clad_radius
+                else:
+                    # Guide tube
+
+                    # First, remove volume of the tube
+                    cell_mod_volume -= np.pi * (cell.outer_radius*cell.outer_radius - cell.inner_radius*cell.inner_radius)
+
+                    # Remove volume of the burnable poison rod (if present)
+                    if isinstance(cell.fill, BurnablePoisonRod):
+                        cell_mod_volume -= np.pi * cell.fill.outer_clad_radius*cell.fill.outer_clad_radius
+
+                        # Add center volume back if the center is moderator !
+                        if cell.fill.center is not None:
+                            cell_mod_volume += np.pi * cell.fill.center_radius * cell.fill.center_radius
+
+                # First check for quarter symmetry and being corner pin
+                if (
+                    self.symmetry == Symmetry.Quarter
+                    and self.shape[0] % 2 == 1
+                    and self.shape[1] % 2 == 1
+                    and j == self._simulated_shape[1] - 1
+                    and i == 0
+                ):
+                    cell_mod_volume *= 0.25
+                # Next, check for being on the side with a half pin in quarter symmetry
+                elif (
+                    self.symmetry == Symmetry.Quarter
+                    and self.shape[0] % 2 == 1
+                    and i == 0
+                ):
+                    cell_mod_volume *= 0.5
+                # Next, check for being on the bottom row with a half pin
+                elif (
+                    self.symmetry != Symmetry.Full
+                    and self.shape[1] % 2 == 1
+                    and j == self._simulated_shape[1] - 1
+                ):
+                    cell_mod_volume *= 0.5
+
+                sum_volume += cell_mod_volume
+
+        if self.symmetry == Symmetry.Quarter:
+            sum_volume *= 4.
+        elif self.symmetry == Symmetry.Half:
+            sum_volume *= 2.
+
+        # Add volume for the gap around the assembly
+        if self.grid_sleeve_width is None:
+            sum_volume += self.assembly_pitch**2. - (self.shape[0]*self.pitch)**2.
+        else:
+            sum_volume += self.assembly_pitch**2. - (self.shape[0]*self.pitch+2.*self.grid_sleeve_width)**2.
+            
+
+        self._moderator_volume_fraction = sum_volume / (
             self.assembly_pitch * self.assembly_pitch
         )
 
